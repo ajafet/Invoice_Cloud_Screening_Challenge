ee63ce2eab783785cdb0f4407487e3ea4fe9d5e5
Angel Delgado <angel.delgado01@utrgv.edu>
2020-09-02 08:48:49 -0500

    Initial Commit

---
+++ Invoice_Cloud_Screening_Challenge/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Invoice_Cloud_Screening_Challenge {
+    class Program {
+        static void Main(string[] args) {
+
+            for (int i = 1; i <= 100; i++) {
+
+                if (i % 3 == 0 && i % 5 == 0) {
+                    Console.WriteLine("InvoiceCloud"); 
+                }
+                else if (i % 3 == 0) {
+                    Console.WriteLine("Invoice"); 
+                } 
+                else if (i % 5 == 0) {
+                    Console.WriteLine("Cloud"); 
+                }
+                else {
+                    Console.WriteLine(i);
+                }
+
+            }
+
+        }
+    }
+}
